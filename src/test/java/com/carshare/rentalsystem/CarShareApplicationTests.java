c18c1be04c95f5eca724c0da590e7c18d96a0aa3
Roman Voynahiy <romanvoynahiy@gmail.com>
2025-04-07 17:49:30 +0300

    Initial commit.

---
+++ src/test/java/com/carshare/rentalsystem/CarShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carshare.rentalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
